b6331dcc4c5dc8dde4bb0b88591009ed94c06e48
danlei27 <danleisantos@hotmail.com>
2020-05-28 12:38:51 -0300

    Http Upload de Arquivo: Request com FormData

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-file.component.scss']
 })
 export class UploadFileComponent implements OnInit {
+  files: Set<File>;
 
-  constructor() { }
+  constructor(private service: UploadFileService) { }
 
   ngOnInit(): void {
   }
@@ -20,9 +22,19 @@ export class UploadFileComponent implements OnInit {
     // document.getElementById('customFileLabel').innerHTML = selectedFiles[0].name;
 
     const fileNames = [];
+    this.files = new Set();
     for (let i = 0; i < selectedFiles.length; i++){
       fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
     }
     document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
   }
+
+  onUpload(){
+    if (this.files && this.files.size > 0){
+      this.service.upload(this.files, 'http://localhost:8000/upload')
+        .subscribe(response => console.log('Upload Cunclúido'));
+    }
+  }
+
 }
